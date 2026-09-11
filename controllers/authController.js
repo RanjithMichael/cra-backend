e9e5133aebda8933c80ea2984f161a9cbf985a9a
Michael-12798 <backiarajranjithmichael@gmail.com>
2026-09-11 20:52:35 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,12 +6,18 @@ const generateToken = (id, role) => {
 };
 
 export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, role} = req.body;
   try {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: "User already exists" });
 
-    const user = await User.create({ name, email, password });
+    
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role: role || "user" 
+    });
     res.status(201).json({
       _id: user._id,
       name: user.name,
